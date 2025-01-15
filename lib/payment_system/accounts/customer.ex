894d2fa21732c0356ee0455a09f41396c17d3908
Joansil <73849389+Joansil@users.noreply.github.com>
2025-01-15 10:08:57 -0300

    Includes a PR template (#2)
    * transaction scheme definition
    
    * costumer scheme definition
    
    * includes PR template

--- lib/payment_system/accounts/customer.ex
+++ lib/payment_system/accounts/customer.ex
@@ -1,20 +1,30 @@
 defmodule PaymentSystem.Accounts.Customer do
   use Ecto.Schema
   import Ecto.Changeset
+  alias PaymentSystem.Payments.Transaction
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
   schema "customers" do
+    field :name, :string
     field :email, :string
     field :external_id, :string
-    field :name, :string
     field :status, :string
+    has_many :transactions, Transaction
 
-    timestamps(type: :utc_datetime)
+    timestamps()
   end
+
+  @valid_statuses ~w(active inactive blocked)
 
   @doc false
   def changeset(customer, attrs) do
     customer
     |> cast(attrs, [:name, :email, :external_id, :status])
-    |> validate_required([:name, :email, :external_id, :status])
+    |> validate_required([:name, :email, :status])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/)
+    |> validate_inclusion(:status, @valid_statuses)
+    |> unique_constraint(:email)
+    |> unique_constraint(:external_id)
   end
 end
