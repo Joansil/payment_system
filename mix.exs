7ea38c4dfffc1ec8880bc60c61932c1afb138e71
Joansil <joansilfs@gmail.com>
2025-01-20 23:07:03 -0300

    add coveralls step in ci/cd

--- mix.exs
+++ mix.exs
@@ -15,7 +15,9 @@ defmodule PaymentSystem.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "test.coveralls.github": :test,
+        test: :test
       ]
     ]
   end
